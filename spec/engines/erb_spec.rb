c8d1084d53d663ee50e51590752b3931ccffe7ab
Kane Baccigalupi <baccigalupi@gmail.com>
2010-01-03 22:15:17 -0800

    erb spec fixes

--- spec/engines/erb_spec.rb
+++ spec/engines/erb_spec.rb
@@ -1,50 +1,60 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "ERB Views" do
+  class Simple < Panorama::View
+    engine_type :erb 
+    requires :name
+  
+    def markup
+      "<h1>Hello <%= self[:name] %></h1>"
+    end
+  end     
+
+  class Titleous < Simple 
+    requires :title
+  
+    def long_name
+      "#{self[:name]}: #{self[:title]}"
+    end
+  
+     def markup
+      "
+      #{super}
+      <p>
+        Your full designation is:
+        <b><%= self.long_name %></b>
+      </p>
+      "
+    end   
+  end      
+
+
   describe Panorama::Engine::ERB do
     it 'should render an empty string' do
       Panorama::Engine::ERB.render("").should == ""
     end
   end 
   
-  class Simple < Panorama::View
-    engine_type :erb 
-    requires :name 
-    
-    def markup
-      "<h1>Hello <%= self[:name] %></h1>" 
-    end
-  end     
-  
-  class Titleous < Simple 
-    requires :name, :title
-    
-    def long_name
-      "#{self[:name]}: #{self[:title]}"
-    end 
-    
-    def markup
-      "
-      #{super}
-      <p>
-        Your full designation is:
-        <b><%= long_name %></b>
-      </p>
-      "
+  describe 'rendering' do
+    describe 'simple' do 
+      it 'should render methods' do 
+        output = Simple.render(:name => 'Kane')  
+        output.should match(/<h1>\s{0,5}Hello\s*Kane\s{0,5}<\/h1>/) 
+      end  
     end  
-  end
+    
+    describe 'inheritance' do 
+      it 'should render local variables' do
+        output = Titleous.render(:name => 'Kane', :title => 'Cog in Wheel')
+        output.should match(/<h1>\s{0,5}Hello\s*Kane\s{0,5}<\/h1>/) 
+      end
   
-  
-  describe 'markup' do
-    it 'should render local variables' do 
-      output = Simple.render(:name => 'Kane')
-      output.should include "<h1>Hello Kane</h1>"
-    end
-  
-    it 'should render methods' do
-      output = Titleous.render(:name => 'Kane', :title => 'Cog in Wheel')
-      output.should include "<h1>Hello Kane</h1>"
-    end 
+    
+      it 'should render inherited methods' do 
+        output = Titleous.render(:name => 'Kane', :title => 'Cog in Wheel')
+        output.should match(/Kane: Cog in Wheel/)
+      end
+    end  
   end 
      
 end   
--- spec/tempalets/simple.rb
+++
@@ -1,8 +0,0 @@
-class Simple < Panorama::View 
-  def markup 
-    h1 "some text"
-    p * "my_class" | "my_id" {
-      b "I love you!"
-    }
-  end  
-end  
